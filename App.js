47c23eeb6afa2eae43c205940b12daefaba2b27d
Caue <cauemagri@gmail.com>
2024-09-09 10:26:34 -0300

    projeto01

--- App.js
+++ App.js
@@ -6,12 +6,15 @@ import TelaNode from "./componentes/TelaNode";
 import TelaReact from "./componentes/TelaReact";
 
 const Menu = createDrawerNavigator();
+
 export default function App() {
   return (
     <NavigationContainer>
+      <Menu.Navigator>
       <Menu.Screen name="Java Script" component={TelaJavaScript}/>
       <Menu.Screen name="React" component={TelaReact}/>
-<Menu.Screen name="Node" component={TelaNode}/>
+      <Menu.Screen name="Node" component={TelaNode}/>
+      </Menu.Navigator>
     </NavigationContainer>
     
   );
